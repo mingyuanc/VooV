ff3e80d01061259d06cfdcf0c50e04ab555cc08a
Ming Yuan <cmingyuan123@gmail.com>
2024-01-28 05:12:29 +0800

    Fix suspense error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Toaster richColors position="bottom-center" expand={false} closeButton />
-      <body className={inter.className}>{children}</body>
+      <Suspense fallback={<div>Loading...</div>}>
+        <body className={inter.className}>{children}</body>
+      </Suspense>
     </html>
   );
 }
