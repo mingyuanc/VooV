5d84a3703decb7841e1eca3f06c7ca583922c853
Ming Yuan <cmingyuan123@gmail.com>
2024-03-31 00:59:04 +0800

    Test8

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-// import { Suspense } from "react";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Toaster richColors position="bottom-center" expand={false} closeButton />
-      {/* <Suspense fallback={<div>Loading...</div>}> */}
-      <body className={inter.className}>{children}</body>
-      {/* </Suspense> */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <body className={inter.className}>{children}</body>
+      </Suspense>
     </html>
   );
 }
