7c7b48bcada6e2d7a9ebe59b408cd5f723e41d3b
Ming Yuan <cmingyuan123@gmail.com>
2024-03-31 01:56:17 +0800

    Revert "Edit page size for mobile"
    This reverts commit 8b922418385b0a7343ec645f415a5ffd0f6292f2.

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -9,7 +9,7 @@ export default function Results() {
   const dest = searchParams.get("ending")!;
 
   return (
-    <div className="max-w-full max-h-full">
+    <div className="max-w-full">
       <ResultsPage src={src} dest={dest} />
     </div>
   );
