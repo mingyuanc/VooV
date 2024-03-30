f89434907e6084e602c40481a342aa26c7af9ef3
Ming Yuan <cmingyuan123@gmail.com>
2024-03-31 00:36:00 +0800

    test23

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -4,9 +4,11 @@ import { useSearchParams } from "next/navigation";
 import ResultsPage from "@/features/results/components/ResultsPage";
 
 export default function Results() {
-  const searchParams = useSearchParams();
-  const src = searchParams.get("starting")!;
-  const dest = searchParams.get("ending")!;
+  // const searchParams = useSearchParams();
+  // const src = searchParams.get("starting")!;
+  // const dest = searchParams.get("ending")!;
+  const src = "";
+  const dest = "";
 
   return (
     <div className="max-w-full">
