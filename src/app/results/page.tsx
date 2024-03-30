dd181ee4c4b54743f6a817517c7ff1b4376bcd4d
Ming Yuan <cmingyuan123@gmail.com>
2024-03-31 01:07:04 +0800

    Test10

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,6 +1,7 @@
+"use client";
+
 import { useSearchParams } from "next/navigation";
 import ResultsPage from "@/features/results/components/ResultsPage";
-import { Suspense } from "react";
 
 export default function Results() {
   const searchParams = useSearchParams();
@@ -8,10 +9,8 @@ export default function Results() {
   const dest = searchParams.get("ending")!;
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="max-w-full">
-        <ResultsPage src={src} dest={dest} />
-      </div>
-    </Suspense>
+    <div className="max-w-full">
+      <ResultsPage src={src} dest={dest} />
+    </div>
   );
 }
